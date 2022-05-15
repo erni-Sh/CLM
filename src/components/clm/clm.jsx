03c476f853ccd20d463e7e5da55a0941cf5dadb3
erni-Sh <ernest22@mail.ru>
2022-05-15 21:15:47 +0300

    first build

--- src/components/clm/clm.jsx
+++ src/components/clm/clm.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import './clm.scss';
+import './diff-layers.scss';
 import './button.scss';
 
 import bgDay from '../../assets/images/bg_day.jpg';
@@ -16,7 +17,7 @@ import textFloor5 from '../../assets/images/text-floor-5.png';
 import textFloor6 from '../../assets/images/text-floor-6.png';
 
 const MAX_WIDTH = 1024; //1024;
-const MAX_STEPS = 8;
+const MAX_STEPS = 10;
 
 export default function CLM() {
 	const [ step, setStep ] = useState(0);
@@ -24,18 +25,19 @@ export default function CLM() {
 	// sequencer
 	const sqncr = {
 		bgDay: [0],
-		bgNight: [7],
+		bgDayOver: [1],
+		bgNight: [8],
 		bgNames: [0, 1],
-		lighthouseDay: [1],
-		lighthouseNight: [7],
+		lighthouseDay: [0],
+		lighthouseNight: [8],
 		templates: [0],
-		textFloor1: [1],
-		textFloor2: [2],
-		textFloor3: [3],
-		textFloor4: [4],
-		textFloor5: [5],
-		textFloor6: [6],
-		//flashing: [8],
+		textFloor1: [2],
+		textFloor2: [3],
+		textFloor3: [4],
+		textFloor4: [5],
+		textFloor5: [6],
+		textFloor6: [7],
+		flashing: [9],
 	}
 
 	const renderStep = (st = 0) => {
@@ -95,19 +97,31 @@ export default function CLM() {
 		}, 2000)
 	})
 
+	// height lighthouse
+	useEffect(()=>{
+		const startMove = 2;
+		const visPositions = [81, 72, 61.5, 50, 38.3, 0];
+
+		if(step > startMove + visPositions.length - 1) return false;
+		const visLHouse = (step < startMove) ? visPositions[0] : visPositions[step - startMove];
+
+		document.querySelector('[data-layer="lighthouseDay"]')
+			.style.clipPath = `polygon( 0 ${visLHouse}%, 100% ${visLHouse}%, 100% 100%, 0 100% )`;
+	}, [step])
+
 	return (
 		<>
 			<div className="clm__wrapper">
 				<div className="clm__inner" style={{maxWidth: MAX_WIDTH}}>
 					<img className="clm__layer" src={bgDay} alt='' data-layer="bgDay"/>
 					<img className="clm__layer" src={bgNight} alt='' data-layer="bgNight"/>
-
 					<img className="clm__layer" src={templates} alt='' data-layer="templates"/>
-					<img className="clm__layer" src={bgNames} alt='' data-layer="bgNames"/>
+
+					<img className="clm__layer layer__bg-names" src={bgNames} alt='' data-layer="bgNames"/>
 
 					<img className="clm__layer" src={lighthouseDay} alt='' data-layer="lighthouseDay"/>
 					<img className="clm__layer" src={lighthouseNight} alt='' data-layer="lighthouseNight"/>
-					<div className="clm__layer" data-layer="flashing"></div>
+					<div className="clm__layer layer__flashing" data-layer="flashing"></div>
 					<img className="clm__layer" src={textFloor6} alt='' data-layer="textFloor6"/>
 					<img className="clm__layer" src={textFloor5} alt='' data-layer="textFloor5"/>
 					<img className="clm__layer" src={textFloor4} alt='' data-layer="textFloor4"/>
